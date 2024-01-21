1cd5c261b6c39b929e629f4620072fdb0ca3f1ee
Abhishek Kumar <abhishekkr1915@gmail.com>
2024-01-21 19:28:14 +0530

    worked on gallary page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CldUploadButton } from 'next-cloudinary';
 import { CldImage } from 'next-cloudinary';
 import { useState } from 'react';
 
-type UploadResult = {
+ export type UploadResult = {
   info: {
     public_id: string;
   },
@@ -13,7 +13,7 @@ type UploadResult = {
 
 
 export default function Home() {
-  const [imageId, setImageId] = useState("")
+  const [imageId, setImageId] = useState("exrkqr4vkit5n8boyqk8")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CldUploadButton
@@ -22,44 +22,14 @@ export default function Home() {
         }}
         uploadPreset="l8x9p7fx" />
       {imageId && <CldImage
-        width="960"
-        height="600"
+        width="400"
+        height="300"
         src={imageId}
         sizes="100vw"
+        tint="70:blue:green:purple"
         alt="Description of my image"
       />}
 
     </main>
   )
 }
-
-
-// {
-//   "event": "success",
-//     "info": {
-//     "id": "uw-file6",
-//       "batchId": "uw-batch5",
-//         "asset_id": "70bec2dd494870a4eb362664c0da106e",
-//           "public_id": "ozv1uyv7sld7ojyxvae3",
-//             "version": 1705751382,
-//               "version_id": "427d8698985f623f9f3ed776ab1fa3dc",
-//                 "signature": "3e801666f06656b5e151012efe0295c745cfcfd2",
-//                   "width": 1231,
-//                     "height": 794,
-//                       "format": "png",
-//                         "resource_type": "image",
-//                           "created_at": "2024-01-20T11:49:42Z",
-//                             "tags": [],
-//                               "bytes": 106456,
-//                                 "type": "upload",
-//                                   "etag": "b2ab5af9f0cb47d3bb8e54c1cf0c0a3e",
-//                                     "placeholder": false,
-//                                       "url": "http://res.cloudinary.com/dgl713axv/image/upload/v1705751382/ozv1uyv7sld7ojyxvae3.png",
-//                                         "secure_url": "https://res.cloudinary.com/dgl713axv/image/upload/v1705751382/ozv1uyv7sld7ojyxvae3.png",
-//                                           "folder": "",
-//                                             "access_mode": "public",
-//                                               "original_filename": "App.jsx - react-otp-login [WSL_ Ubuntu] - Visual Studio Code 15-01-2024 10_06_19",
-//                                                 "path": "v1705751382/ozv1uyv7sld7ojyxvae3.png",
-//                                                   "thumbnail_url": "https://res.cloudinary.com/dgl713axv/image/upload/c_limit,h_60,w_90/v1705751382/ozv1uyv7sld7ojyxvae3.png"
-//   }
-// }
